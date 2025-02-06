bcb0fe89549adf49734b5a185bed8c179e42b883
221B501 <221B501@juet.co.in>
2025-02-06 15:37:04 +0530

    .

--- le3/Problem3/RubberDuck.java
+++
@@ -1,9 +0,0 @@
-public class RubberDuck extends Duck implements Swimmable, Squeakable, Mute {
-    public void squeak() {
-        System.out.println("Rubber Duck squeaks.");
-    }
-
-    public void mute() {
-        System.out.println("Rubber Duck is mute.");
-    }
-}
--- le3/Problem3/WoodenDuck.java
+++
@@ -1,5 +0,0 @@
-public class WoodenDuck extends Duck implements Swimmable, Mute {
-    public void mute() {
-        System.out.println("Wooden Duck is mute.");
-    }
-}
