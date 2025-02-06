b00bbad621b7a1d8bf9c96b4925bd4622192cdf7
MananJain501 <221b501@juetguna.in>
2025-02-06 15:33:59 +0530

    Delete le3 directory

--- le3/Problem2/AbsTest.java
+++
@@ -1,3 +0,0 @@
-public abstract class AbsTest implements Testable {
-
-}
--- le3/Problem3/Duck.java
+++
@@ -1,5 +0,0 @@
-public abstract class Duck implements Swimmable {
-    public void swim() {
-        System.out.println("Duck is swimming.");
-    }
-}
